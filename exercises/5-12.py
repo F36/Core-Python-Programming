1c927382e0e8583b8b899c9a6bd2b1b4e248bec3
Kylin Roc <kylinroc@gmail.com>
2015-03-26 20:18:14 +0800

    Add exercise 5-12

---
+++ exercises/5-12.py
@@ -0,0 +1,11 @@
+import sys
+
+print sys.maxint
+print type(sys.maxint)
+print type(sys.maxint + 1)
+print -sys.maxint - 1
+print type(-sys.maxint - 1)
+print type(-sys.maxint - 2)
+
+print sys.float_info.max
+print sys.float_info.min
