fb7e3fbe9a13928f374b116b4d43bb6f4171a660
Kylin Roc <kylinroc@gmail.com>
2015-04-01 20:12:13 +0800

    Add exercise 6-3

---
+++ exercises/6-3.py
@@ -0,0 +1,9 @@
+list_of_numbers = [2, 4, 5, 6, 2, 6, 5, 8, 9, 3]
+print list_of_numbers
+list_of_numbers.sort()
+print list_of_numbers
+
+list_of_strings = ['OS X', 'Apple', 'Windows', 'Google']
+print list_of_strings
+list_of_strings.sort(None, None, True)
+print list_of_strings
